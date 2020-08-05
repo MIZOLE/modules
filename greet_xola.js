9d8d2fb2d4c5039e565f0c6e744162b77273f5d8
codex123 <you@example.com>
2020-08-05 11:26:49 +0200

    commit greet

--- greet_xola.js
+++ greet_xola.js
@@ -1,17 +1,17 @@
-// const chalk = require('chalk');
+ const chalk = require('chalk');
 // //import the greet module that is in the current folder
-// const greet = require('./greet');
+ const greet = require('./greet');
 
-// const styledMessage = chalk.bgGreen.black(greet('Xola'));
-// console.log(styledMessage)
+
 
 var figlet = require('figlet');
  
-figlet('Hello Lulama!!', function(err, data) {
+figlet(greet('Xola'), function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data)
+     const styledMessage = chalk.bgGreen.black(data);
+ console.log(styledMessage)
 });
